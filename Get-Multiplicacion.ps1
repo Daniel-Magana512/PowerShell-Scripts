c8acfcace9213a4f827822f5e7a80bc1886f0598
Fabian Escobar <fabiannorbertoescobar@gmail.com>
2017-08-19 19:08:06 -0300

    Get-Multiplicacion

---
+++ Get-Multiplicacion.ps1
@@ -0,0 +1,35 @@
+﻿function Get-Multiplicacion()
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline = $true,
+                   ValueFromPipelineByPropertyName = $true)]
+        [int]$value
+    )
+
+   Begin
+   {
+        $result = 1
+   }
+   Process
+   {
+        Write-Output "Resultado parcial: $result"
+        
+        if($value -lt 0)
+        {
+            $result = 0
+        }
+        elseif($value -le 1)
+        {
+            $result = 1
+        }
+        else
+        {
+            $result = $value * $result
+        }                
+   }
+   End
+   {
+        Write-Output "Resultado final: $result"
+   }
+}
