1bb9c28b5ea5efb5ccb4d9722529a6813ade6840
Fabian Escobar <fabiannorbertoescobar@gmail.com>
2017-08-19 19:08:32 -0300

    Detener-Proceso

---
+++ Detener-Proceso.ps1
@@ -0,0 +1,21 @@
+﻿function Detener-Proceso()
+{
+    param(
+    [Parameter(Mandatory=$true,
+               ValueFromPipeline=$true,
+               ValueFromPipelineByPropertyName=$true)]
+    [Int[]]$id
+    )
+
+    Process {
+        Foreach($proc in $id) {
+            try {
+                kill -id $proc
+                Write-Output "El proceso $proc fue detenido exitosamente"
+            }
+            catch {
+                Write-Warning "El proceso $proc no existe en el sistema"
+            }
+        }
+    }
+}
