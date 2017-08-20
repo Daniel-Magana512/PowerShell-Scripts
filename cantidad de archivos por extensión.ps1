560080032874bade24ee01269675bf22fc5d79d5
Fabian Escobar <fabiannorbertoescobar@gmail.com>
2017-08-20 14:01:45 -0300

    Cantidad de archivos por extensión

---
+++ cantidad de archivos por extensión.ps1
@@ -0,0 +1,10 @@
+﻿<#
+    .Synopsis
+    Cantidad de archivos en el disco D
+    .Description
+    Se muestra en un GridView la cantidad de archivos que hay de cada tipo según su extensión,
+    en el disco D, en todos los directorios,
+    ordenados de forma decreciente por cantidad.
+#>
+
+ls "D:\" -File -Recurse | Group-Object Extension | Sort-Object Count -Descending | Select-Object Name, Count | Out-GridView
