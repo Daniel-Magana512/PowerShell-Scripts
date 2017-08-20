a6baad7940a509269ecb4c92cd9fb3b251fa0ae3
Fabian Escobar <fabiannorbertoescobar@gmail.com>
2017-08-20 14:46:45 -0300

    Procesos del sistema

---
+++ procesos del sistema.ps1
@@ -0,0 +1,8 @@
+﻿<#
+    .Synopsis
+    Procesos del sistema
+    .Description
+    Se muestra el id, nombre y horario de arranque, y tiempo total en procesador de cada proceso del sistema
+#>
+
+Get-Process | Select-Object Id, ProcessName, Starttime, TotalProcessorTime
