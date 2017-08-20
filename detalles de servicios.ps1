ee1c34c465407e4c99ae68c8dc544bfc76caba7f
Fabian Escobar <fabiannorbertoescobar@gmail.com>
2017-08-20 15:08:10 -0300

    Detalles del sistema

---
+++ detalles de servicios.ps1
@@ -0,0 +1,8 @@
+﻿<#
+    .Synopsis
+    Servicios del sistema
+    .Description
+    Servicios del sistema del sistema ordenados por nombre
+#>
+
+Get-Service | Sort-Object Name | Format-List Name, DisplayName, Status, CanStop, CanPauseAndContinue, CanShutdown, StartType
