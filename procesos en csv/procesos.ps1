b95c72c655420988b00fbaf4f5227cf164a480f7
Fabian Escobar <fabiannorbertoescobar@gmail.com>
2017-08-20 16:48:38 -0300

    Procesos en csv

---
+++ procesos en csv/procesos.ps1
@@ -0,0 +1,11 @@
+﻿<#
+    .Synopsis
+    Procesos del sistema
+    .Description
+    Se exportan los procesos del sistema a un archivo procesos.csv.
+    Se importa el archivo procesos.csv y se muestra en pantalla
+#>
+
+Get-Process | Select-Object Id, ProcessName, Starttime, CPU | Export-Csv "D:\UNLaM\SISTEMAS OPERATIVOS\FABIAN\POWERSHELL\procesos en csv\procesos.csv" -Delimiter ','
+
+Import-Csv "D:\UNLaM\SISTEMAS OPERATIVOS\FABIAN\POWERSHELL\procesos en csv\procesos.csv" -Delimiter ','
