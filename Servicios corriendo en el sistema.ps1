71233fc4c31bae3891d31374ffc35a9aaa0a32f4
Fabian Escobar <fabiannorbertoescobar@gmail.com>
2017-08-20 13:43:34 -0300

    Servicios corriendo en el sistema

---
+++ Servicios corriendo en el sistema.ps1
@@ -0,0 +1,9 @@
+﻿<#
+    .Synopsis
+    Servicios corriendo en el sistema
+    .Description
+    Se muestra en un GridView todos los servicios que están corriendo en el sistema, ordenados por nombre desplegado.
+    Para cada servicio se muestra el nombre del servicio y su nombre desplegado
+#>
+
+Get-Service | Where-Object { $_.Status -eq "Running" } | Sort Displayname | Select-Object ServiceName, DisplayName | Out-GridView
